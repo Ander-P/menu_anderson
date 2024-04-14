8cbb7b0fd01d46809e8e7345547825cd8cc18598
Ander-P <pradoanderson1203@outlook.com>
2024-04-13 19:11:59 -0500

    feat: menu nav with fragments

---
+++ app/src/main/java/com/example/framentsmenu/FragmentoSpinner.java
@@ -0,0 +1,41 @@
+package com.example.framentsmenu;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class FragmentoSpinner extends Fragment {
+    Spinner spnTest;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_spinner, container, false);
+        spnTest = view.findViewById(R.id.spnTest);
+
+        ArrayAdapter spnAdapter = ArrayAdapter.createFromResource(
+                this.getContext(),
+                R.array.spinner_items,
+                android.R.layout.simple_spinner_item
+        );
+
+        spnAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+
+        spnTest.setAdapter(spnAdapter);
+
+        return view;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/framentsmenu/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.framentsmenu;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    FrameLayout frmHome;
+
+    BottomNavigationView btmNav;
+
+    ImageViewFragment imageViewFragment = new ImageViewFragment();
+    FragmentoSpinner spinnerFragment = new FragmentoSpinner();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadFragment(imageViewFragment);
+
+        frmHome = findViewById(R.id.frmHome);
+        btmNav = findViewById(R.id.btmNav);
+
+        btmNav.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.navHome) {
+                loadFragment(imageViewFragment);
+                return true;
+            } else if (item.getItemId() == R.id.navChats) {
+                loadFragment(spinnerFragment);
+                return true;
+            }
+
+            return false;
+        });
+    }
+
+    public void loadFragment(Fragment fr) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frmHome, fr);
+        transaction.commit();
+    }
+}
